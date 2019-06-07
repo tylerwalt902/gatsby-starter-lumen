c0fb9fdd823260ef57f5b1a0a68443dbbe45badf
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-07 13:52:13 +0545

    Update Content.js

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -12,7 +12,7 @@ const Content = ({ body, title }: Props) => (
     <h1 className={styles['content__title']}>Watch One Piece {title} Online.</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
     <div className={styles['content__body']}>
-      Watch One Piece {title} online in high quality. Watch One Piece {title} Subbed Online. Watch One Piece {title} in 720p quality. 
+      Watch One Piece {title} online in high quality. Watch One Piece {title} Subbed Online. Watch One Piece {title} in best quality. 
       Watch {title} of one piece in best quality. Download One Piece {title} in high quality.
     </div>
   </div>
