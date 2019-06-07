fca0d43197b26c5ea753c784eea1048a5154ef5b
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-07 14:40:06 +0545

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   url: 'https://1piece.me',
   title: '1Piece - Watch One Piece Episodes Online For Free',
   subtitle: 'Watch latest one piece episodes for free online in high quality updated weekly in HD.',
-  copyright: '© Aeonanime. All rights reserved. Copyrights and trademarks for the anime, and other promotional materials are held by their respective owners and their use is allowed under the fair use clause of the Copyright Law.',
-  disqusShortname: 'aeonanime',
+  copyright: '© 1Piece.me. All rights reserved. Copyrights and trademarks for the anime, and other promotional materials are held by their respective owners and their use is allowed under the fair use clause of the Copyright Law.',
+  disqusShortname: '1Piece.me',
   postsPerPage: 50,
   googleAnalyticsId: 'UA-73379983-2',
   menu: [
